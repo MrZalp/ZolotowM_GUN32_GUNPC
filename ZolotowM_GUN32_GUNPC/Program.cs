24006bcb2dad857db60f21ece187c0a814e0b114
Neto test <zolotowced@gmail.com>
2024-10-31 19:33:44 +0300

    Добавьте файлы проекта.

---
+++ ZolotowM_GUN32_GUNPC/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        if (!int.TryParse(Console.ReadLine(), out int a))
+        {
+            Console.WriteLine("Error! Invalid input.");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("Введите второе число:");
+        if (!int.TryParse(Console.ReadLine(), out int b))
+        {
+            Console.WriteLine("Error! Invalid input.");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("Введите оператор (&, |, ^):");
+        string operation = Console.ReadLine();
+        int result;
+
+        switch (operation)
+        {
+            case "&":
+                result = a & b;
+                break;
+            case "|":
+                result = a | b;
+                break;
+            case "^":
+                result = a ^ b;
+                break;
+            default:
+                Console.WriteLine("Error! Invalid operator.");
+                return;
+        }
+
+        Console.WriteLine($"десятичный: {result}");
+        Console.WriteLine($"Двоичный: {Convert.ToString(result, 2)}");
+        Console.WriteLine($"Шестнадцатеричный: {result:X}");
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
+
+
